760472387d49f6c28abf621e5d135a97b1e3beff
cgdesignermm <cgdesigner.mm@gmail.com>
2025-11-08 17:03:40 +0700

    Save Data test

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const taskInput = document.getElementById('new-task');
+const taskInput = document.getElementById('newTask');
 const taskList = document.getElementById('task-list');
 const addtaskbtn = document.getElementById('addtaskbtn');
+const cache = [];
+
 
 // Check Input
 function checkInput() {
@@ -11,24 +13,25 @@ function checkInput() {
 function createTask() {
     const taskText = checkInput();
     if (taskText === '') return;
-    
+
     const listItem = document.createElement('li');
     const taskTextSpan = document.createElement('span');
     const deleteBtn = document.createElement('button');
-    
+
     taskTextSpan.textContent = taskText;
     deleteBtn.textContent = 'Delete';
     deleteBtn.className = 'delete-btn';
-    
+
     // Add delete functionality to this specific button
-    deleteBtn.addEventListener('click', function() {
+    deleteBtn.addEventListener('click', function () {
         taskList.removeChild(listItem);
     });
-    
+    cache.push(taskText);
+    console.log(cache);
     listItem.appendChild(taskTextSpan);
     listItem.appendChild(deleteBtn);
     taskList.appendChild(listItem);
-    
+
     // Clear input after adding task
     taskInput.value = '';
 }
@@ -45,10 +48,44 @@ function enterTaskCreate(event) {
     }
 }
 
-// Event Listener Click & Enter
+function resetData(){
+    cache=[];
+    displaySavedData();
+}
+function saveData() {
+    //Save the Input Task to localstorage
+    localStorage.setItem('saveData', cache);
+
+    //Update the displayed saved data
+    displaySavedData();
+    console.log(cache);
+}
+
+function displaySavedData() {
+    // console.log("displaySavedData");
+    const saveData = localStorage.getItem('saveData');
+    const savedDataElement = document.getElementById('savedData');
+    if (saveData) {
+        savedDataElement.textContent = saveData;
+    } else {
+        savedDataElement.textContent = 'Nothing to do yet.';
+    }
+}
+
+//Display saved Data
+window.onload = displaySavedData;
+
+
+
+
+
+// Event Listener Click & Enter & Save
 addtaskbtn.addEventListener('click', clickTaskCreate);
 taskInput.addEventListener('keypress', enterTaskCreate);
+saveDataBtn.addEventListener('click', saveData);
 
-function Test() {
-    console.log("Got It!");
-}
+// function Test() {
+//     console.log("Got It!");
+// }
+
+
